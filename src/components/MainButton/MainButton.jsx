a0434bee3a5c11eb328f2205863b47748a050f7f
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-04-26 22:35:34 +0200

    fix lint errors

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
 const MainButton = ({ text }) => (
-  <button className={styles.mainButton}>
+  <button type="button" className={styles.mainButton}>
     {text}
   </button>
 );
 
 MainButton.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
 };
 
 export default MainButton;
--- src/components/SideButton/SideButton.jsx
+++ src/components/SideButton/SideButton.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
 const SideButton = ({ text }) => (
-  <button className={styles.sideButton}>
+  <button type="button" className={styles.sideButton}>
     {text}
   </button>
 );
 
 SideButton.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
 };
 
 export default SideButton;
