3a2276f0eed385c4017324d91f10439dbd2c3c7c
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-05-17 18:37:55 +0200

    add mobile nav menu

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Circles from '../Circles/Circles';
 import Nav from '../Nav/Nav';
 import styles from './styles.module.scss';
+import MobileNav from '../MobileNav/MobileNav';
 
 const HomePage = () => (
   <div className={styles.home}>
     <Nav />
     <Circles />
+    <MobileNav />
   </div>
 );
 
---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+
+const routes = [
+  { routeName: 'Home', route: '/' },
+  { routeName: 'Services', route: '/services' },
+  { routeName: 'Portfolio', route: '/portfolio' },
+  { routeName: 'People', route: '/people' },
+];
+
+const MobileNav = () => {
+  const router = useRouter();
+  return (
+    <nav className={styles.mobileNav}>
+      {routes.map(({ routeName, route }) => (
+        <Link key={route} href={route}>
+          <a className={cn(styles.link, {
+            [styles.activeLink]: route === router.route,
+          })}
+          >
+            {routeName}
+          </a>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+export default MobileNav;
